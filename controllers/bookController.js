c87bab4dd990dcc364ac7ddc1c48ecafc0b63fee
wouerner <wouerner@protonmail.com>
2021-11-08 13:43:56 -0300

    add update, del, store in controller

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,4 +1,24 @@
-exports.index = function(req, res) {
-    const books = [{"name" : "book"}, {"name" : "book 2"}];
+exports.index = (req, res)=> {
+    const books = [{"name" : "book 1"}, {"name" : "book 2"}];
+    res.json(books);
+};
+
+exports.show = function(req, res) {
+    const books = {"name" : "book"};
+    res.json(books);
+};
+
+exports.store = function(req, res) {
+    const books = {"message": "Criado!", "data":{"name" : "book"}};
+    res.json(books);
+};
+
+exports.update = function(req, res) {
+    const books = {"message": "Atualizado!", "data":{"name" : "book"}};
+    res.json(books);
+};
+
+exports.del = function(req, res) {
+    const books = {"message": "Excluido!", "data":{"name" : "book"}};
     res.json(books);
 };
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,10 @@ var router = express.Router();
 var bookController = require('../controllers/bookController');
 
 router.get('/',  bookController.index)
+router.get('/show',  bookController.show)
+router.post('/store',  bookController.store)
+router.put('/update',  bookController.update)
+router.delete('/del',  bookController.del)
 
 module.exports = router;
 
