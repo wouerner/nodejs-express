9b9fb58e86d6fbd9200840abcfed80a8faf651ce
wouerner <wouerner@protonmail.com>
2021-11-23 18:15:21 -0300

    Add tests

--- tests/book.test.js
+++ tests/book.test.js
@@ -1,6 +1,51 @@
-describe('Testing the test', () => {
-  it('should test that true === true', () => {
-    expect(true).toBe(true)
-  })
+const request = require('supertest');
+const app = require('../app');
+
+describe("GET Book", () => {
+  it("should return 200 and JSON", () => {
+    return request(app)
+      .get("/")
+      .expect('Content-Type', /json/)
+      .expect(200, [{ name: 'book 1'}, { name: 'book 2'}] )
+      ;
+  });
 })
 
+describe("GET SHOW Book", () => {
+  it("should return 200 and JSON", () => {
+    return request(app)
+      .get("/show")
+      .expect('Content-Type', /json/)
+      .expect(200, { name: 'book'} )
+      ;
+  });
+})
+
+describe("POST Book", () => {
+  it("should return 200 and JSON", () => {
+    return request(app)
+      .post("/")
+      .expect('Content-Type', /json/)
+          .expect(200, { message: 'Criado!', data: { name: 'book' } } )
+      ;
+  });
+})
+
+describe("PUT Book", () => {
+  it("should return 200 and JSON", () => {
+    return request(app)
+      .put("/")
+      .expect('Content-Type', /json/)
+      .expect(200, { message: 'Atualizado!', data: { name: 'book' }});
+  });
+})
+
+describe("DELETE Book", () => {
+  it("should return 200 and JSON", () => {
+    return request(app)
+      .delete("/")
+      .expect('Content-Type', /json/)
+          .expect(200, { message: 'Excluido!', data: { name: 'book' }} )
+      ;
+  });
+})
